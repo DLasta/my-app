646e19b5caa801cdda84154cc6233bde76162c1a
DL <dario.lasta123@gmail.com>
2021-08-18 19:49:42 +0200

    class

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  render() {return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+}}
 
 export default App;
